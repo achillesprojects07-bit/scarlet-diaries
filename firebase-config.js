037fb0665897fdcde3325bd3b9b94241b753f7bf
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-06-09 03:23:18 +0300

    Update and rename firebase-config.example.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD629-PqZPG5T4yyGngUXEpcen7vEHdPWM",
+  authDomain: "scarlet-diaries.firebaseapp.com",
+  projectId: "scarlet-diaries",
+  storageBucket: "scarlet-diaries.firebasestorage.app",
+  messagingSenderId: "208475329990",
+  appId: "1:208475329990:web:5598ab44c5744bb0a4cec7"
+};
